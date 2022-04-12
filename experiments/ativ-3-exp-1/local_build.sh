cb312402f226c650716f233f0d8613cf5781c22b
João Seródio <joaoserodio@live.com>
2022-04-12 15:12:40 -0300

    add local_* scripts (build, prepare_data and experiment)

---
+++ experiments/ativ-3-exp-1/local_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+release=true
+while getopts :d flag
+do
+    case "${flag}" in
+        d) release=false;;
+    esac
+done
+
+if $release; then
+    build_path="build_release"
+    flags="-DGMX_BUILD_OWN_FFTW=ON"
+else
+    build_path="build_debug"
+    flags="-DGMX_BUILD_OWN_FFTW=ON -DCMAKE_BUILD_TYPE=Debug"
+fi
+
+mkdir -p $build_path
+
+cd $build_path
+
+cmake ../../.. $flags
+
+make -j 2
---
+++ experiments/ativ-3-exp-1/local_experiment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+samples=10;
+data_path="data";
+results_path="results"
+
+while getopts s:p:r: flag
+do
+    case "${flag}" in
+        s) samples=$OPTARG;;
+        p) data_path=$OPTARG;;
+        r) results_path=$OPTARG;;
+    esac
+done
+
+mkdir -p $results_path
+OLD_PATH=$PATH
+
+# Release
+export PATH=$(pwd)/build_release/bin:$OLD_PATH
+rm $results_path/release.csv
+for counter in $(seq 1 $samples); do
+    echo Starting Release Run $counter of $samples
+    gmx mdrun -v -deffnm $data_path/em 2>/dev/null | grep "MO833" | cut -d " " -f 5 >> $results_path/release.csv
+    echo Release Runs: $counter/$samples
+done
+
+#Debug
+export PATH=$(pwd)/build_debug/bin:$OLD_PATH
+rm $results_path/debug.csv
+for counter in $(seq 1 $samples); do
+    echo Starting Debug Run $counter of $samples
+    gmx mdrun -v -deffnm $data_path/em 2>/dev/null | grep "MO833" | cut -d " " -f 5 >> $results_path/debug.csv
+    echo Debug Runs: $counter/$samples
+done
